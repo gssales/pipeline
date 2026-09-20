105e50f1aefbebc7295ad9e60f3ebfa1db9b0511
Guilherme Sales <guilherme.sales@inf.ufrgs.br>
2026-09-20 13:01:59 -0300

    envgs

--- main-envgs.py
+++ main-envgs.py
@@ -21,67 +21,7 @@ import shlex
 
 from monitor_utils import get_vram_procs, monitor
 from process_utils import ProcessManager
-
-# Load datasets and parameters
-def read_scenes(dataset_path: Path):
-  scenes = []
-  if dataset_path.exists() and dataset_path.is_dir():
-    for scene in os.listdir(dataset_path):
-      scene_path = dataset_path / scene
-      if scene_path.exists() and scene_path.is_dir():
-        scenes.append(scene_path)
-  return scenes
-
-def get_dataset_args(dataset, stage, datasets, parameters):
-  dataset_args = ""
-  if stage not in parameters["parameters"]:
-    return ""
-  if "real" in parameters["parameters"][stage] and dataset in datasets["data"]["real_datasets"]:
-    dataset_args += parameters["parameters"][stage]["real"]
-  if "synthetic" in parameters["parameters"][stage] and dataset in datasets["data"]["synthetic_datasets"]:
-    dataset_args += parameters["parameters"][stage]["synthetic"]
-  return dataset_args
-
-def load_datasets(args):
-  scenes = []
-  datasets = {}
-  with open("params/datasets.yaml", 'r') as file:
-    try:
-      datasets = yaml.safe_load(file)
-      basePath = Path(datasets["data"]["base_path"], "easyvolcap")
-      if not args.synthetic_scenes_only:
-        for dataset in datasets["data"]["real_datasets"]:
-          scenes.extend(read_scenes(basePath / dataset))
-      if not args.real_scenes_only:
-        for dataset in datasets["data"]["synthetic_datasets"]:
-          scenes.extend(read_scenes(basePath / dataset))
-        
-    except yaml.YAMLError as exc:
-      print(exc)
-  return scenes, datasets
-
-def load_parameters(method):
-  params_path = Path(f"params/scene_args_{method}.yaml")
-  if not params_path.exists():
-    print(f"Parameters file {params_path} not found. Please make sure it exists and is named correctly.")
-    exit(1)
-
-  parameters = {}
-  with open(params_path, 'r') as file:
-    try:
-      parameters = yaml.safe_load(file)
-    except yaml.YAMLError as exc:
-      print(exc)
-  return parameters
-
-@contextmanager
-def cd(destination):
-  old = os.getcwd()
-  os.chdir(destination)
-  try:
-    yield
-  finally:
-    os.chdir(old)
+from utils.parser_utils import load_datasets, load_scene_args
 
 
 ################
@@ -106,9 +46,9 @@ def training(args, eval_dir, scene, datasets, parameters):
 
   scene_times = {}
   scene_time = time.time()
-  process = psutil.Popen(shlex.split(train_command), cwd=parameters["script_path"], shell=False)
+  process = psutil.Popen(shlex.split(train_command), cwd=parameters["working_directory"], shell=False)
   pm.process = process
-  pm.start_monitor(monitor, process.pid, active_gpu_procs, 1.0, os.path.join(output_path, "usage.csv"))
+  pm.start_monitor(monitor, process.pid, active_gpu_procs, 10.0, os.path.join(output_path, "usage.csv"))
   try:
     process.wait()
   finally:
@@ -162,46 +102,67 @@ def render_videos(args, eval_dir, parameters):
 
 def pipeline(args):
   
-  scenes, datasets = load_datasets(args)
-  params = load_parameters(args.method)
+  datasets = load_datasets(args.datasets_config)
+  params = load_scene_args(args.scene_args_config_path, args.method)
+  
+  if args.datasets:
+    datasets = {k: v for k, v in datasets.items() if k in args.datasets}
+    if not datasets:
+      print("No valid datasets found for the provided dataset IDs.")
+      exit(1)
 
   eval_dir = Path(args.output_dir) if args.output_dir else Path(params["base_path"], "eval_" + time.strftime("%Y%m%d-%H%M%S"))
 
-  for scene in scenes:
-    if not args.skip_training:
-      training(args, eval_dir, scene, datasets, params)
+  stages = args.stages if args.stages else ["training"]
 
-    # if not args.skip_rendering:
-    #   rendering(args, eval_dir, scene, datasets, params)
+  for dataset_id, dataset in datasets.items():
+    if args.real_scenes_only and not dataset.get("real", False):
+      continue
+    if args.synthetic_scenes_only and dataset.get("real", False):
+      continue
     
-    # if not args.skip_fps:
-    #   fps_evaluation(args, eval_dir, scene, datasets, params)
+    evaluate_normal_mae = dataset.get("has_normals", False) and params["method"].get("evaluate_normal_mae", True)
 
-    # if not args.skip_metrics:
-    #   metrics_evaluation(args, eval_dir, scene, params)
+    scenes = dataset["scenes"]
+    print(f"Evaluating dataset: {dataset_id} with {len(scenes)} scenes.")
 
-    # if params.get("mae_eval_datasets", False) and not args.skip_mae_eval:
-    #   mae_evaluation(args, eval_dir, scene, params)
+    if args.filter_scenes_by:
+      scenes = [scene for scene in scenes if scene.name in args.filter_scenes_by]
+      print(f"Filtered scenes to {len(scenes)} based on provided filter: {args.filter_scenes_by}")
 
-    # if not args.skip_collect_results:
-    #   collect_results(eval_dir)
+    for scene in scenes:
+      if "training" in stages:
+        training(args, eval_dir, scene, datasets, params)
+
+      # if not args.skip_rendering:
+      #   rendering(args, eval_dir, scene, datasets, params)
+      
+      # if not args.skip_fps:
+      #   fps_evaluation(args, eval_dir, scene, datasets, params)
+
+      # if not args.skip_metrics:
+      #   metrics_evaluation(args, eval_dir, scene, params)
+
+      # if params.get("mae_eval_datasets", False) and not args.skip_mae_eval:
+      #   mae_evaluation(args, eval_dir, scene, params)
+
+      # if not args.skip_collect_results:
+      #   collect_results(eval_dir)
 
   print("Done with full evaluation for all scenes!")
 
 if __name__ == "__main__":
   parser = ArgumentParser(description="Full evaluation script parameters")
-  parser.add_argument("--skip_training", action="store_true")
-  parser.add_argument("--skip_rendering", action="store_true")
-  parser.add_argument("--skip_fps", action="store_true")
-  parser.add_argument("--skip_metrics", action="store_true")
-  parser.add_argument("--skip_collect_results", action="store_true")
-  parser.add_argument("--skip_render_videos", action="store_true")
-  parser.add_argument("--skip_mae_eval", action="store_true")
+  parser.add_argument("--datasets_config", default="./params/datasets.yaml", help="Path to the datasets configuration YAML file.")
+  parser.add_argument("--scene_args_config_path", default="./params", help="Path to the scene arguments configuration YAML file.") 
+  parser.add_argument("--method", default="envgs")
+  parser.add_argument("--output_dir", default=None)
+  parser.add_argument("--stages", nargs='+', default=["training"], help="Stages to run. Options: 'training', 'rendering', 'metrics', 'mae_eval', 'fps', 'render_videos', 'collect'.")
   parser.add_argument('--real_scenes_only', action='store_true')
   parser.add_argument('--synthetic_scenes_only', action='store_true')
   parser.add_argument("--dry_run", action="store_true", help="If set, the script will print the commands that would be run without executing them.")
-  parser.add_argument("--output_dir", default=None)
-  parser.add_argument("--method", default="envgs")
+  parser.add_argument("--datasets", nargs='+', help="List of dataset IDs to filter and run the evaluation on. If not provided, all datasets will be evaluated.")
+  parser.add_argument("--filter_scenes_by", nargs='+', default=[], help="List of scene names to filter and run the evaluation on. If not provided, all scenes will be evaluated.")
   args, _ = parser.parse_known_args()
 
   if args.real_scenes_only and args.synthetic_scenes_only:
